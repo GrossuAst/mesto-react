0666730d44c4feeece2b68d5bc25135e0f089bbe
Maxim Grossu <maxgrossu@yandex.ru>
2023-05-02 21:50:22 +0300

    добавлен файл api

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,8 @@ import Footer from "./Footer";
 import PopupWithForm from "./PopupWithForm";
 import ImagePopup from "./ImagePopup";
 
+// const css = {marginTop: '50px'}
+
 function App() {
 
   const [isEditProfilePopupOpen, setEditProfilePopupVisible] = React.useState(false);
@@ -28,6 +30,12 @@ function App() {
     setAvatarPopupVisible(true);
   }
 
+  function closeAllPopups() {
+    setEditProfilePopupVisible(false);
+    setAddPlacePopupVisible(false);
+    setAvatarPopupVisible(false);
+  }
+
   return (
     <div className='wrapper'>
       <div className="content">
@@ -42,27 +50,71 @@ function App() {
 
         <Footer />
 
+        {/* попап аватарки */}
         <PopupWithForm
           name='avatar' 
           isOpen={isEditAvatarPopupOpen}
           title='Обновить аватар'
           buttonText='Сохранить'
-          placeholder='Ссылка на аватар'
-        />
+          onClose={closeAllPopups}
+        > 
+          <input
+            className="popup__input"
+            placeholder='Ссылка на аватар'
+            // style={css}
+          />
+          <span 
+            className="popup__error"
+          />
+        </PopupWithForm>
 
+        {/* попап формы профиля */}
         <PopupWithForm 
           name='profile'
           isOpen={isEditProfilePopupOpen}
           title='Редактировать профиль'
           buttonText='Сохранить'
-        />
+          onClose={closeAllPopups}
+        > 
+          <input 
+            name='name'
+            className="popup__input"
+          />
+          <span 
+            className="popup__error"
+          />
+          <input 
+            name="about"
+            className="popup__input"
+          />
+          <span 
+            className="popup__error"
+          />
+        </PopupWithForm>
 
+        {/* попап формы добавления карточки */}
         <PopupWithForm 
           name='add-card'
           isOpen={isAddPlacePopupOpen}
           title='Новое место'
           buttonText='Сохранить'
-        />
+          onClose={closeAllPopups}
+        >
+          <input 
+            name="title"
+            className="popup__input"
+          />
+          <span 
+            className="popup__error"
+          />
+          <input 
+            name="link"
+            className="popup__input"
+          />
+          <span 
+            className="popup__error"
+          />
+        </PopupWithForm>
       </div>
     </div>
   );
--- src/components/PopupWithForm/index.js
+++ src/components/PopupWithForm/index.js
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { Children } from "react";
 
 function PopupWithForm(props) {
     return(
         <div className={`popup popup_type_${props.name} ${props.isOpen ? "popup_opened" : "" }` }>
             <div className="popup__container">
                 <form action="" className="popup__form" name={props.name}>
-                    <button type="button" aria-label="#" className="popup__close-icon" ></button>
+                    <button type="button" aria-label="#" className="popup__close-icon" onClick={props.onClose}></button>
                     <h2 className="popup__title">{props.title}</h2>
+                    {props.children}                   
                     <button type="submit" className="popup__submit-button">{props.buttonText}</button>
                 </form>
             </div>
