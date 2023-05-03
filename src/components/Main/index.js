e7cefaeb48f72e8fb9f49dabe26874ed0ddfc85d
Maxim Grossu <maxgrossu@yandex.ru>
2023-05-03 20:18:23 +0300

    апи запрос для вывода данных профиля

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -1,6 +1,28 @@
 import React from 'react';
+import { api } from '../../utils/api';
 
 function Main({onEditProfile, onAddPlace, onEditAvatar}) {
+    
+    const [userName, setUserName] = React.useState('');
+    const [userDescription, setUserDescription] = React.useState('');
+    const [userAvatar, setUserAvatar] = React.useState('');
+    const [cards, setCardsArray] = React.useState([]);
+
+    React.useEffect(() => {
+        Promise.all([api.getInfoAboutUser(), api.getInitialCards()])
+        .then(([userInfo, cards]) => {
+        setUserName(userInfo.name)
+        setUserDescription(userInfo.about)
+        setUserAvatar(userInfo.avatar)
+        setCardsArray(cards)
+        // console.log(cards)
+    } 
+  )
+  .catch((err) => {
+    console.log(`ошибка ${err}`);
+  })
+
+    })
     
 
     return (
@@ -9,11 +31,12 @@ function Main({onEditProfile, onAddPlace, onEditAvatar}) {
                 <div className="profile__redact">
                     <button className="profile__avatar-overlay"
                         onClick={onEditAvatar}
+                        style={{ backgroundImage: `url(${userAvatar})` }}
                     >
-                        <img src="#" alt="фото пользователя" className="profile__avatar" />
+                        {/* <div src="#" alt="фото пользователя" className="profile__avatar" style={{ backgroundImage: `url(${userAvatar})` }}></div> */}
                     </button>
-                    <h1 className="profile__name">Жак-Ив Кусто</h1>
-                    <p className="profile__description">Исследователь океана</p>
+                    <h1 className="profile__name">{userName}</h1>
+                    <p className="profile__description">{userDescription}</p>
                     <button type="button" aria-label="кнопка открытия попапа редактирования профиля" 
                         className="profile__edit-button"
                         onClick={onEditProfile}
@@ -26,20 +49,18 @@ function Main({onEditProfile, onAddPlace, onEditAvatar}) {
                     >
                 </button>
             </section>
-            <section className="elements">
-                <template id="card-template">
-                    <article className="card">
-                        <button type="button" aria-label="кнопка удаления карточки" className="card__delete-button"></button>
-                        <img src="#" alt="#" className="card__photo" />
-                        <div className="card__description">
-                            <h3 className="card__title">#</h3>
-                            <div className="card__likes-section">
-                                <button type="button" aria-label="кнопка переключения лайка" className="card__like"></button>
-                                <span className="card__likes-counter">0</span>
-                            </div>
+            <section className="elements">               
+                <article className="card">
+                    <button type="button" aria-label="кнопка удаления карточки" className="card__delete-button"></button>
+                    <img src="#" alt="#" className="card__photo" />
+                    <div className="card__description">
+                        <h3 className="card__title">#</h3>
+                        <div className="card__likes-section">
+                            <button type="button" aria-label="кнопка переключения лайка" className="card__like"></button>
+                            <span className="card__likes-counter">0</span>
                         </div>
-                    </article>
-                </template>
+                    </div>
+                </article>
             </section>
         </main>
     );
