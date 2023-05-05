a75981822bf6eae78225308367399398ea408419
Maxim Grossu <maxgrossu@yandex.ru>
2023-05-05 17:18:03 +0300

    исправлены ошибки

--- src/components/ImagePopup.js
+++ src/components/ImagePopup.js
@@ -4,7 +4,7 @@ function ImagePopup({card, onClose}) {
     return(
         <div className={`popup popup_type_fullscreen ${card ? "popup_opened" : "" }`}>
             <article className="popup__fullsceen-container">
-                <img src={card?.link} alt="" className="popup__fullscreen-image"/>
+                <img src={card?.link} alt={card?.name} className="popup__fullscreen-image"/>
                 <h3 className="popup__fullscreen-title">{card?.name}</h3>
                 <button type="button" aria-label="закрыть фуллскрин фото" className="popup__close-icon popup__close-icon_type_fullscreen"
                     onClick={onClose}
